302c8d18ddce3797e53e5aa3425d48564f59eaf3
Yangjing <yangjing@wordoor.com>
2019-01-25 13:29:33 +0800

    no message

--- AVSpeechSynthesizerDemo/ViewController.m
+++ AVSpeechSynthesizerDemo/ViewController.m
@@ -24,6 +24,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.textView.editable = NO;
 }
 
 - (IBAction)confirmAction:(id)sender {
